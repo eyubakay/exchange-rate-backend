832c1cd728c8bc40d1f0aeecc360e723d51caf07
Eyüp Akay <eyup.akay@vodafone.com>
2021-09-27 13:50:13 +0300

    improved controller

--- exchange-rate-service/src/main/java/com/integratedfinance/exchangerateservice/app/controller/ExchangeApiController.java
+++ exchange-rate-service/src/main/java/com/integratedfinance/exchangerateservice/app/controller/ExchangeApiController.java
@@ -4,10 +4,12 @@ import com.integratedfinance.exchangerateservice.app.aop.annotation.DailyExchang
 import com.integratedfinance.exchangerateservice.app.aop.annotation.Loggable;
 import com.integratedfinance.exchangerateservice.app.api.ExchangeApi;
 import com.integratedfinance.exchangerateservice.app.entity.ExchangeRate;
+import com.integratedfinance.exchangerateservice.app.exception.RecordNotFoundException;
 import com.integratedfinance.exchangerateservice.app.model.ExchangeRateRequest;
 import com.integratedfinance.exchangerateservice.app.model.ExchangeRateResponse;
 import com.integratedfinance.exchangerateservice.app.service.ExchangeRepositoryService;
 import com.integratedfinance.exchangerateservice.app.util.CurrencyConverterUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +21,7 @@ import java.util.Objects;
  * @author Eyüp Akay
  */
 @RestController
+@Slf4j
 public class ExchangeApiController implements ExchangeApi {
 
     @Autowired
@@ -30,8 +33,13 @@ public class ExchangeApiController implements ExchangeApi {
     public ResponseEntity<ExchangeRateResponse> getExchangeRate(ExchangeRateRequest request) {
         ExchangeRate latestRate = exchangeRepositoryService.getLatest();
 
-        if (Objects.isNull(latestRate) || Objects.isNull(request.getSourceCurrency()) || Objects.isNull(request.getTargetCurrency())) {
-            throw new RuntimeException(); //TODO
+        if (Objects.isNull(request.getSourceCurrency()) || Objects.isNull(request.getTargetCurrency())) {
+            log.info("PARAMETERS CANNOT BE EMPTY");
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
+
+        if (Objects.isNull(latestRate)) {
+            throw new RecordNotFoundException("Unexpectedly failed due to cannot find exchange rate record");
         }
         ExchangeRateResponse response = new ExchangeRateResponse();
         response.setExchangeRateDate(latestRate.getDate());
